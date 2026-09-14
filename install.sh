5ec52803de8ccf56c477082d1bfd35faf6bbfade
Gus Mamakos <cjm394@cornell.edu>
2026-09-14 14:28:18 -0400

    added preset

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ set -e
 # starship prompt
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
+
+# apply a startship preset
+mkdir -p ~/.config
+starship preset gruvbox-rainbow -o ~/.config/starship.toml
