8b37e1773019d9eb1d3d33a1d62e2b51217a94de
Gus Mamakos <49040689+gusmamakos@users.noreply.github.com>
2026-09-13 23:03:46 -0400

    Remove git aliases from install.sh
    Removed git aliases configuration from install script.

--- install.sh
+++ install.sh
@@ -4,6 +4,3 @@ set -e
 # starship prompt
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
-
-# git aliases / config, if you want
-git config --global alias.lg "log --oneline --graph --all"
